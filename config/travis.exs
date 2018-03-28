445411d129a9605a844ef751e074e3e6c6299bf5
supersubwoofer@gmail.com <supersubwoofer@gmail.com>
2018-03-28 15:52:02 -0400

    move travis.exs to folder config

--- config/travis.exs
+++ config/travis.exs
@@ -13,7 +13,7 @@ config :logger, level: :warn
 config :my_app, MyApp.Repo,
   adapter: Ecto.Adapters.Postgres,
   username: "postgres",
-  password: "postgres",
+  password: "",
   database: "my_app_test",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
